59911625d084b972402c6c9072a0f3dc2a1586ab
Jongok Yoon <bak01076@gmail.com>
2020-11-12 20:37:01 +0100

    getmovies, getmovie and get suggestions

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,7 +1,9 @@
-import {  getMovies } from "./db"
-const resolvers =  {
-    Query: {
-        movies: (_,{limit, rating}) => getMovies(limit, rating)
-    }
+import { getMovie, getMovies, getSuggestions } from "./db";
+const resolvers = {
+  Query: {
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
+  },
 };
 export default resolvers;
